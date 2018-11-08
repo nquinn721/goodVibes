de10a1b8f092c031121c7485e2c7c05d6b5fd29a
Nate <natethepcspecialist@gmail.com>
2018-11-08 17:33:02 -0500

    update

--- server/app.js
+++ server/app.js
@@ -6,6 +6,9 @@ const express = require('express'),
 const DB = require('./db');
 const Yelp = require('./apis/yelp');
 
+app.get('/', (req, res) => {
+	res.send('Thanks for visiting Good Vibes Technologies!');
+});
 app.get('/:lat/:lon', (req, res) => {
 	console.log(req.params);
 	Yelp.search({term: 'dispensary', lat: req.params.lat, lon: req.params.lon}, (data) => {
