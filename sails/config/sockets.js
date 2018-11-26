b3db6e931c47f843781406ba17146fbc2167872e
Nathan <natethepcspecialist@gmail.com>
2018-11-26 15:24:19 -0500

    'update'

--- sails/config/sockets.js
+++ sails/config/sockets.js
@@ -10,7 +10,7 @@
  */
 
 module.exports.sockets = {
-
+  onlyAllowOrigins: ["http://good-vibes-tech.herokuapp.com"],
   /***************************************************************************
   *                                                                          *
   * `transports`                                                             *
